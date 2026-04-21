7e68b5c3c936780c425abc16cd99d1177873d94b
kamun <Kamun4277@gmail.com>
2026-04-21 11:28:06 +0800

    Initial commit

---
+++ Assets/script1.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class script1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Hello world");
+        print("THis is a script!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
